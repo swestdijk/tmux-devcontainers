990f8704866eb64c5428d0ec15825f1bee9fcac3
Phil Balchin <phil@maniacalrobot.co.uk>
2025-04-01 09:11:46 +0100

    Updated docker compose command to include containers in non-running states

--- scripts/status.sh
+++ scripts/status.sh
@@ -5,7 +5,7 @@ if [ -f ./.devcontainer/devcontainer.json ]; then
     # workspace_name=$(devcontainer read-configuration --workspace-folder . | jq ".configuration.name")
     # workspace_name=${workspace_name//\"/} # remove quotes
 
-    echo $(docker compose ps --format json | jq -r '. | "\(.Service):\(.State)"')
+    echo $(docker compose ps --all --format json | jq -r '. | "\(.Service):\(.State)"')
 else
     # Workspace does not have devcontainers
     echo "N/A"
